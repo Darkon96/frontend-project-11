a5d401096e20ea48b35180b922663063f5790e31
Darkon96 <steponframe2@gmail.com>
2024-02-08 17:48:56 +0300

    17:48_08.02.2024

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,26 +1,26 @@
 export default {
-    translation: {
-      text: {
-        header: 'RSS agregator',
-        slogan: 'Start reading RSS today! It\'s easy, it\'s beautiful.',
-        example: 'Example: https://ru.hexlet.io/lessons.rss',
-        label: 'RSS link',
-        posts: 'Posts',
-        feeds: 'Feeds',
-        submitButton: 'Add',
-        readButton: 'Preview',
+  translation: {
+    text: {
+      header: 'RSS agregator',
+      slogan: 'Start reading RSS today! It\'s easy, it\'s beautiful.',
+      example: 'Example: https://ru.hexlet.io/lessons.rss',
+      label: 'RSS link',
+      posts: 'Posts',
+      feeds: 'Feeds',
+      submitButton: 'Add',
+      readButton: 'Preview',
+    },
+    feedback: {
+      error: {
+        urlRequired: 'Must not be empty',
+        notValidURL: 'Link must be a valid URL',
+        alreadyExists: 'RSS already exists',
+        netError: 'Network error',
+        parseError: 'The resource does not contain valid RSS',
       },
-      feedback: {
-        error: {
-          urlRequired: 'Must not be empty',
-          notValidURL: 'Link must be a valid URL',
-          alreadyExists: 'RSS already exists',
-          netError: 'Network error',
-          parseError: 'The resource does not contain valid RSS',
-        },
-        success: {
-          feedAdded: 'RSS uploaded successfully',
-        },
+      success: {
+        feedAdded: 'RSS uploaded successfully',
       },
     },
-  };
+  },
+};
